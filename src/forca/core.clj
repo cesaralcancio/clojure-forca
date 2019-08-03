f0b4f5c52e30800c922d775e531f2aebe4d7866b
CesarAlcancio <cesar.alcancio@payclip.com>
2019-08-03 16:37:54 -0500

    First version

---
+++ src/forca/core.clj
@@ -0,0 +1,41 @@
+(ns forca.core
+  (:gen-class))
+
+(def total-de-vidas 6)
+
+(defn perdeu [] (print "Você perdeu"))
+(defn ganhou [] (print "Você ganhou"))
+
+(defn letras-faltantes [palavras acertos]
+  (remove (fn [letra] (contains? acertos (str letra))) palavras)
+)
+
+(defn acertou-a-palavra-toda? [palavra acertos]
+  (empty? (letras-faltantes palavra acertos))
+)
+
+(defn le-letra! [] (read-line))
+
+(defn acertou? [chute palavra] (.contains palavra chute))
+
+(defn avalia-chute [chute vidas palavra acertos]
+    (if (acertou? chute palavra)
+        (jogo vidas palavra (conj acertos chute))
+        (jogo (dec vidas) palavra acertos)
+    )
+)
+
+(defn jogo [vidas palavra acertos]
+    (if (= vidas 0) 
+        (perdeu)
+        (if (acertou-a-palavra-toda? palavra acertos)
+            (ganhou)
+            (avalia-chute (le-letra!) vidas palavra acertos)
+        )
+    )
+)
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
