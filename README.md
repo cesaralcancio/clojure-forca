266514e1800197a979b5c69486cab3b0e40d0462
CesarAlcancio <cesar.alcancio@payclip.com>
2019-08-03 19:00:01 -0500

    Finalizado

--- README.md
+++ README.md
@@ -18,7 +18,29 @@ FIXME: listing of options this app accepts.
 
 ## Examples
 
-...
+* Instalar o REPL: http://leiningen.org/#install
+* Para habilitar a execucao clojure "lein repl".
+* Para compilar o clojure: (require '[forca.core :as forca] :reload)
+* Executar o projeto: (forca/jogo 3 "CESAR" #{})
+* def num [1 2 3] - Declarar uma variavel
+* defn mult [ x] (* x 2) - Declarar uma funcao
+* defn par [ x] (= 0 (rem x 2)) - Declarar uma funcao utilizando o resto
+* map e remove - Funcoes
+* (defn numeros [1, 2, 3, 4, 5]) - Cria a array numeros
+* (filter (fn [x] (or (< x 2) (> x 4))) numeros) - Filtra todos do conjunto e tira quem nao passa
+* (map (fn [x] (* x 3)) numeros) - Roda a funcao para cada numero da array
+* (remove (fn [x] (= 1 (rem x 2))) numeros) - Remove numeros impares
+* (reduce (fn [a b] (- b a)) numeros ) - Passa a primeira posicao do array no a e passa a segunda posicao do array no b, retorna so 1 valor
+* (def palavra #{"A" "L" "U" "R" "O" "E"})
+* (filter (fn [x] (or (= x "A") (= x "O"))) palavra) - Verifica se a palavra tem A ou O
+* (->> numeros (map (fn [x] (* x 2))) (map (fn [x] (+ x 1))) (reduce (fn [acc next] (+ acc next)))) - Executa varias funciones em sequencia
+* Ambos fazem a mesma coisa, porem o segundo e mais legivel:
+* (map (fn[x] (- x 200.0)) (map (fn[x] (* x 2.0)) carros))
+* (->> carros (map (fn[x] (* x 2))) (map (fn[x] (- x 200))))
+* Para rodar o jogo (rodar o main): lein run
+* Criar arquivo JAR: lein uberjar
+* java -jar forca-0.1.0-SNAPSHOT-standalone.jar
+* 
 
 ### Bugs
 
--- src/forca/core.clj
+++ src/forca/core.clj
@@ -3,39 +3,45 @@
 
 (def total-de-vidas 6)
 
+(def palavra-secreta "MELANCIA")
+
 (defn perdeu [] (print "Você perdeu"))
 (defn ganhou [] (print "Você ganhou"))
 
 (defn letras-faltantes [palavras acertos]
-  (remove (fn [letra] (contains? acertos (str letra))) palavras)
-)
+  (remove (fn [letra] (contains? acertos (str letra))) palavras))
 
 (defn acertou-a-palavra-toda? [palavra acertos]
-  (empty? (letras-faltantes palavra acertos))
-)
+  (empty? (letras-faltantes palavra acertos)))
 
 (defn le-letra! [] (read-line))
 
 (defn acertou? [chute palavra] (.contains palavra chute))
 
-(defn avalia-chute [chute vidas palavra acertos]
-    (if (acertou? chute palavra)
-        (jogo vidas palavra (conj acertos chute))
-        (jogo (dec vidas) palavra acertos)
-    )
-)
+(defn imprime-forca [vidas palavra acertos]
+    (println "Vidas " vidas)
+    (doseq [letra (seq palavra)]
+        (if (contains? acertos (str letra)) 
+            (print letra " ") 
+            (print "_" " ")))
+    (println))
 
 (defn jogo [vidas palavra acertos]
-    (if (= vidas 0) 
-        (perdeu)
-        (if (acertou-a-palavra-toda? palavra acertos)
-            (ganhou)
-            (avalia-chute (le-letra!) vidas palavra acertos)
-        )
-    )
-)
+    (imprime-forca vidas palavra acertos)
+    (cond 
+        (= vidas 0) (perdeu)
+        (acertou-a-palavra-toda? palavra acertos) (ganhou)
+        :else
+        (let [chute (le-letra!)]
+            (if (acertou? chute palavra)
+                (do 
+                    (println "Acertou a letra!")
+                    (recur vidas palavra (conj acertos chute)))
+                (do    
+                    (println "Errou a letra! Perdeu vida!")
+                    (recur (dec vidas) palavra acertos))))))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn comeca-o-jogo [] (jogo total-de-vidas palavra-secreta #{}))
+
+(defn -main [& args]
+  (comeca-o-jogo))
